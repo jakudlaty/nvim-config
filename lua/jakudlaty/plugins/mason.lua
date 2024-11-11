83348bd5bcb3557900a48fa5f8e9b9e4de628d0f
jakudlaty <jakudlaty@gmail.com>
2024-11-11 11:00:31 +0100

    mason

--- lua/jakudlaty/plugins/mason.lua
+++ lua/jakudlaty/plugins/mason.lua
@@ -25,7 +25,6 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
         "html",
         "cssls",
         "tailwindcss",
